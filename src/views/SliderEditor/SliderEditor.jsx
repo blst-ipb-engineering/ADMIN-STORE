6790970547366f34250f9c99ff3783960fda55ee
rakapradika <30821261+rakapradika@users.noreply.github.com>
2021-01-29 11:13:59 +0700

    Update SliderEditor.jsx

--- src/views/SliderEditor/SliderEditor.jsx
+++ src/views/SliderEditor/SliderEditor.jsx
@@ -296,7 +296,7 @@ class SlideEditor extends Component {
 
     // Replace cloudinary upload URL with yours
     return axios
-      .post("https://api.cloudinary.com/v1_1/pradika2010/image/upload", formData, {
+      .post("https://api.cloudinary.com/v1_1/pradika2010", formData, {
         headers: { "X-Requested-With": "XMLHttpRequest" }
       })
       .then(response => {
